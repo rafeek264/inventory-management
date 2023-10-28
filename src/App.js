4eae309cbee5c14eca32f0603534831413f7da3b
rafeek264 <rafeekraf123@gmail.com>
2023-10-28 15:50:19 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+function App() {
+  return (
+    <View style={{flex:1, justifyContent:"center", alignItems:"center" , backgroundColor:"#ccffcc"}}>
+      <Text style={{fontSize:20, fontWeight:"bold"}}>Hi i am a react native component</Text>
+    </View>
+  );
+}
+
+export default App;
